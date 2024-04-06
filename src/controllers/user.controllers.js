ea3c0a52fec8ca27c88b1faad1ab4e59a68f4bf7
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-04-06 14:06:41 +0530

    Added Login user and access and refresh token sent

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,33 +1,103 @@
-import mongoose from 'mongoose';
-import {asyncHandler} from "../utils/asyncHandlers.js";
-import {apiError} from "../utils/apiErrors.js";
-import {apiResponse} from "../utils/apiResponse.js";
-import {User} from "../models/user.models.js";
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandlers.js";
+import { apiError } from "../utils/apiErrors.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { User } from "../models/user.models.js";
 
 // Register User
-const registerUser = asyncHandler(async(req, res)=>{
-    const {userName, email, password} = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  const { userName, email, password } = req.body;
 
-    const existedUser = await User.findOne({email})
+  const existedUser = await User.findOne({ email });
 
-    if(existedUser){
-        throw new apiError(400, "Email already exists");
-    }
+  if (existedUser) {
+    throw new apiError(400, "Email already exists");
+  }
 
-    const user = await User.create({
-        userName,
-        email,
-        password
-    });
+  const user = await User.create({
+    userName,
+    email,
+    password,
+  });
 
-    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-    if(!createdUser){
-        throw new apiError(500, "Something went wrong while creating user")
-    }
+  if (!createdUser) {
+    throw new apiError(500, "Something went wrong while creating user");
+  }
 
-    return res
-     .status(201)
-     .json(new apiResponse(200, createdUser, "User registered successfully"))
-})
+  return res
+    .status(201)
+    .json(new apiResponse(200, createdUser, "User registered successfully"));
+});
 // Register User
+
+// Generate Access and Refresh Token
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: true });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new apiError(
+      500,
+      "Something went wrong while generating access and refresh token"
+    );
+  }
+};
+// Generate Access and Refresh Token
+
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    throw new apiError(400, "Email is required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new apiError(400, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new apiError(401, "Password is incorrect");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new apiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+// Login User
+
+export { registerUser };
