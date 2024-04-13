70ea6994b7bd5064a11400808eb2be4b43587ef0
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-04-13 17:50:23 +0530

    Added methods in cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://descrify.netlify.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
