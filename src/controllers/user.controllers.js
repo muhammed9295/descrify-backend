1314ac40e45f1b5656c2e14b9148cebae077518e
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-04-13 15:08:31 +0530

    Adjusted sameSite to lax

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -84,7 +84,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: 'lax',
   };
 
   return res
@@ -116,7 +116,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: 'lax',
   };
 
   return res
